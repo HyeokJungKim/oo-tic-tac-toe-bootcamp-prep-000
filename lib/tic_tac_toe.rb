d38192fd1ba88d53c1ac14e54016b03916a5027a
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-01-09 05:07:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,11 +47,7 @@ end
     end
 
     def current_player
-      if turn_count % 2 == 0
-        return "X"
-      else
-        return "O"
-      end
+      turn_count % 2 == 0 ? "X" : "O"
     end
 
 end
